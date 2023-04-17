1c38a0ca43c9304acd9e226f200631164d8e10d1
Eugen Ciur <eugen@papermerge.com>
2023-04-17 07:18:34 +0200

    Return 200 OK on valid token, and 401 on invalid token

--- tivoli/main.py
+++ tivoli/main.py
@@ -1,24 +1,36 @@
-import os
-from typing import Annotated
-
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-
 from jose import JWTError, jwt
 
-app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer()
+from fastapi import Request, Response, FastAPI, HTTPException, status
+from fastapi.security.utils import get_authorization_scheme_param
 
-SECRET_KEY = os.environ['PAPERMERGE__SECURITY__SECRET_KEY']
-ALGORITHM = os.environ['PAPERMERGE__SECURITY__TOKEN_ALGORITHM']
+from .config import Settings
+
+
+app = FastAPI()
+settings = Settings()
 
 
 @app.get("/")
-async def root(token: Annotated[str, Depends(oauth2_scheme)]) -> None:
+async def root(request: Request) -> Response:
+    authorization = request.headers.get("Authorization")
+    scheme, token = get_authorization_scheme_param(authorization)
+    if not authorization or scheme.lower() != "bearer":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
     try:
-        jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        jwt.decode(
+            token,
+            settings.papermerge__security__secret_key,
+            algorithms=[settings.papermerge__security__token_algorithm]
+        )
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token",
         )
+
+    return Response(status_code=status.HTTP_200_OK)
